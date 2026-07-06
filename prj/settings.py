7319e27c56c406de3ae7a614386f7767293278f6
kajakgm <kajakgm@gmail.com>
2026-07-06 20:20:16 +0530

    Update settings.py

--- prj/settings.py
+++ prj/settings.py
@@ -42,13 +42,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware
+    
 ]
 
 ROOT_URLCONF = 'prj.urls'
@@ -118,6 +119,11 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR , 'media')
 
